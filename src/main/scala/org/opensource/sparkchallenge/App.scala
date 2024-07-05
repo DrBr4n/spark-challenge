b7fd34d71adc03a65746bc239cad247b639ac07d
brunoSilva <brunocunhasilva2001@gmail.com>
2024-07-05 21:23:41 +0100

    feat: part1

--- src/main/scala/org/opensource/sparkchallenge/App.scala
+++ src/main/scala/org/opensource/sparkchallenge/App.scala
@@ -1,5 +1,8 @@
 package org.opensource.sparkchallenge
 
+import org.apache.spark.sql.functions.{avg, col}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
 /**
  * @author Bruno Silva
  */
@@ -7,8 +10,32 @@ package org.opensource.sparkchallenge
 object App {
 
   def main(args : Array[String]): Unit = {
-    println("Hello World")
+    val spark = SparkSession.builder
+      .master("local[*]")
+      .appName("spark-challenge")
+      .getOrCreate()
 
+    val googlePlayStoreUserReviewsDf = spark.read
+      .option("header", "true")
+      // default value is STOP_AT_DELIMITER which causes commas inside quotes to be read as a delimiter
+      .option("unescapedQuoteHandling", "STOP_AT_CLOSING_QUOTE")
+      .csv("src/main/resources/googleplaystore_user_reviews.csv")
+
+    val df_1 = part1(googlePlayStoreUserReviewsDf)
+
+    spark.stop()
+  }
+
+  private def part1(inputDf: DataFrame): DataFrame = {
+
+    val df = inputDf.select("App", "Sentiment_Polarity")
+      .na.replace("Sentiment_Polarity", Map("nan" -> "0"))
+      .withColumn("Sentiment_Polarity", col("Sentiment_Polarity").cast("Double"))
+      .groupBy("App")
+      .avg("Sentiment_Polarity")
+      .withColumnRenamed("avg(Sentiment_Polarity)", "Average_Sentiment_Polarity")
+
+    return df
   }
 
 }
