7ed2eb5d9dd1d09cd9882e4ea93c2344feeced5f
brunoSilva <brunocunhasilva2001@gmail.com>
2024-07-07 02:00:41 +0100

    feat: part3
    fix: option unescapedQuoteHandling wasn't handling all cases
    
    change: changed functions to public to be able to use them more easily in tests

--- src/main/scala/org/opensource/sparkchallenge/App.scala
+++ src/main/scala/org/opensource/sparkchallenge/App.scala
@@ -1,6 +1,6 @@
 package org.opensource.sparkchallenge
 
-import org.apache.spark.sql.functions.{avg, col, desc}
+import org.apache.spark.sql.functions.{array_distinct, col, collect_list, desc, max, max_by, regexp_extract, to_timestamp, when}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 /**
@@ -13,28 +13,31 @@ object App {
     val spark = SparkSession.builder
       .master("local[*]")
       .appName("spark-challenge")
+      .config("spark.sql.legacy.timeParserPolicy", "LEGACY")
       .getOrCreate()
 
     val googlePlayStoreDf = spark.read
       .option("header", "true")
-      // default value is STOP_AT_DELIMITER which causes commas inside quotes to be read as a delimiter
-      .option("unescapedQuoteHandling", "STOP_AT_CLOSING_QUOTE")
+      .option("quote", "\"")
+      .option("escape", "\"")
       .csv("src/main/resources/googleplaystore.csv")
 
     val googlePlayStoreUserReviewsDf = spark.read
       .option("header", "true")
-      // default value is STOP_AT_DELIMITER which causes commas inside quotes to be read as a delimiter
-      .option("unescapedQuoteHandling", "STOP_AT_CLOSING_QUOTE")
+      .option("quote", "\"")
+      .option("escape", "\"")
       .csv("src/main/resources/googleplaystore_user_reviews.csv")
 
     val df_1 = part1(googlePlayStoreUserReviewsDf)
 
     val df_2 = part2(googlePlayStoreDf)
 
+    val df_3 = part3(googlePlayStoreDf)
+
     spark.stop()
   }
 
-  private def part1(inputDf: DataFrame): DataFrame = {
+  def part1(inputDf: DataFrame): DataFrame = {
 
     val df = inputDf
       .na.replace("Sentiment_Polarity", Map("nan" -> "0"))
@@ -46,7 +49,7 @@ object App {
     df
   }
 
-  private def part2(inputDf: DataFrame): DataFrame = {
+  def part2(inputDf: DataFrame): DataFrame = {
 
     // NOTE: The Life Made Wi-Fi Touchscreen Photo Frame app does not have a category,
     //       so values shift left by one, leaving it with the best rating because it has 19 reviews.
@@ -60,9 +63,46 @@ object App {
       .write
       .mode("overwrite")
       .option("delimiter", "§")
+      .option("quote", "\"")
+      .option("escape", "\"")
       .csv("output/best_aps")
 
     df
   }
 
+  def part3(inputDf: DataFrame): DataFrame = {
+
+    var df = inputDf
+      .na.replace(inputDf.columns, Map("NaN" -> "null"))
+      .na.replace("Reviews", Map("null" -> "0"))
+      .na.replace(Array("App", "Category"), Map("null" -> ""))
+      .withColumn("Rating", col("Rating").cast("Double"))
+      .withColumn("Reviews", col("Reviews").cast("Long"))
+      .withColumn("Size",
+        when(col("Size").contains("M"), regexp_extract(col("Size"), "\\d+(\\.\\d+)?", 0).cast("Double"))
+          .otherwise(when(col("Size").contains("k"), regexp_extract(col("Size"), "\\d+(\\.\\d+)?", 0).cast("Double") / 1024)
+              .otherwise(when(col("Size").contains("G"), regexp_extract(col("Size"), "\\d+(\\.\\d+)?", 0).cast("Double") * 1024))))
+      .withColumn("Price",
+        when(col("Price").contains("$"), regexp_extract(col("Price"), "\\d+(\\.\\d+)?", 0).cast("Double") * 0.9)
+          .otherwise(0.0))
+      .withColumn("Last Updated", to_timestamp(col("Last Updated"), "MMMM dd, yyyy"))
+
+    df = df
+      .groupBy("App")
+      .agg(array_distinct(collect_list("Category")).as("Categories"),
+        max_by(col("Rating"), col("Reviews")).as("Rating"),
+        max(col("Reviews")).as("Reviews"),
+        max_by(col("Size"), col("Reviews")).as("Size"),
+        max_by(col("Installs"), col("Reviews")).as("Installs"),
+        max_by(col("Type"), col("Reviews")).as("Type"),
+        max_by(col("Price"), col("Reviews")).as("Price"),
+        max_by(col("Content Rating"), col("Reviews")).as("Content_Rating"),
+        array_distinct(collect_list("Genres")).as("Genres"),
+        max_by(col("Last Updated"), col("Reviews")).as("Last_Updated"),
+        max_by(col("Current Ver"), col("Reviews")).as("Current_Version"),
+        max_by(col("Android Ver"), col("Reviews")).as("Minimum_Android_Version"),
+      )
+
+    df
+  }
 }
